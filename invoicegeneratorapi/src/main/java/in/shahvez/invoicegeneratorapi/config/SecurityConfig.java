29ca4cf7818627f18eb7fcebb5d46b63ad215209
its-shahvez <151781104+its-shahvez@users.noreply.github.com>
2025-08-17 23:49:03 +0530

    Update backend with cors origin

--- invoicegeneratorapi/src/main/java/in/shahvez/invoicegeneratorapi/config/SecurityConfig.java
+++ invoicegeneratorapi/src/main/java/in/shahvez/invoicegeneratorapi/config/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig {
 
     private UrlBasedCorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("https://invoice-genere.netlify.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
